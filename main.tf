8e6fc29d72e0a9946fd8022af9770b45b7d78127
Chris Longe <chris.longe@capitalone.com>
2017-06-08 20:49:27 -0400

    main.tf now creates hello world server.

--- main.tf
+++ main.tf
@@ -2,12 +2,33 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "amazon-linux" {
-    ami             = "ami-c58c1dd3"
+resource "aws_instance" "ubuntu-server" {
+    ami             = "ami-80861296"
     instance_type   = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+    /*
+    The <<-EOF and EOF are Terraform’s heredoc syntax, which allows you to create multi‐
+    line strings without having to insert newline characters all over the place.
+    */
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags {
         Name = "terraform-example"
     }
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
